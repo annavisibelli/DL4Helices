eaa72af307799697e45291ce4a773119902b1a7e
annavisibelli <65961306+annavisibelli@users.noreply.github.com>
2020-05-27 12:20:13 +0200

    Update Word2Vec_Encoding.py

--- Word2Vec_Encoding.py
+++ Word2Vec_Encoding.py
@@ -4,8 +4,8 @@ import gensim
 import numpy as np
 import pandas as pd
 
-de=pd.read_csv("Data/3H.csv",sep=";",header=None).T
-dn=pd.read_csv("Data/Not_Helices.csv",sep=";",header=None).T
+de=pd.read_csv("../Data/3H.csv",sep=";",header=None).T
+dn=pd.read_csv("../Data/Non_Helices.csv",sep=";",header=None).T
 de=de.append(dn,ignore_index=True)
 
 sentences=de.values.tolist()
@@ -21,7 +21,7 @@ model.train(sentences,total_examples=model.corpus_count,epochs=5000)
 model.save('./model5.txt')
 
 #Reload the helices dataset
-de=pd.read_csv("Data/3H.csv",sep=";",header=None).T.values
+de=pd.read_csv("../Data/3H.csv",sep=";",header=None).T.values
 dataset=[]
 for i in range(0,de.shape[0]):
     string=np.zeros(de.shape[1]*5)
@@ -30,10 +30,10 @@ for i in range(0,de.shape[0]):
     dataset.append(string)
 de=pd.DataFrame(dataset)
 prova=(de.loc[0])
-de.to_csv("Data/Helicesw2v.csv",index=False)
+de.to_csv("../Data/Helicesw2v.csv",index=False)
 
 #Reload the Not-helices dataset
-de=pd.read_csv("Data/Not_Helices.csv",sep=";",header=None).T.values
+de=pd.read_csv("../Data/Non_Helices.csv",sep=";",header=None).T.values
 dataset=[]
 for i in range(0,de.shape[0]):
     string=np.zeros(de.shape[1]*5)
@@ -41,7 +41,7 @@ for i in range(0,de.shape[0]):
         string[j*5:(j+1)*5]=model[de[i][j]]
     dataset.append(string)
 de=pd.DataFrame(dataset)
-de.to_csv("Data/Not_Helicesw2v.csv",index=False)
+de.to_csv("../Data/Non_Helicesw2v.csv",index=False)
 
 #Reload the model
 w2vec = gensim.models.Word2Vec.load("./model5.txt")
